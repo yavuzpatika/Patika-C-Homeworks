538a4998ee5f508078a5706ded43b8a0398592e9
yavuzpatika <114187938+yavuzpatika@users.noreply.github.com>
2022-09-23 21:19:01 +0300

    if else homework

---
+++ if else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PatikaHomeWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N % 2 == 1)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N >= 2 && N <= 5 && N % 2 == 0)
+            {
+                Console.WriteLine("Not Weird");
+
+            }
+            else if (N >= 6 && N <= 20 && N % 2 == 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N > 20 && N % 2 == 0)
+            {
+                Console.WriteLine("Not Weird");
+            }
+
+        }
+    }
+}
