2b552a17c693371e0fdf9b61b70fdd1657f25123
yavuzpatika <114187938+yavuzpatika@users.noreply.github.com>
2022-11-07 02:42:24 +0300

    addSoru2andSoru3

---
+++ AlgoritmaSorulari/soru2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AlgoritmaSoruları
+{
+    class birinciSoru
+    {
+        static void Main(string[] args)
+        {
+            /*Soru 2: Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
+           Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+           Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın. */
+
+            Console.Write("kaç adet sayı girmek istediğinizi yazınız: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("eşitlik veya bölme şartı koyacağınız sayıyı yazınız: ");
+            int m = int.Parse(Console.ReadLine());
+
+            int[] dizi = new int[n];
+            Console.WriteLine(n + " adet sayı giriniz");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Lütfen " + (i + 1) + ".pozitif sayıyı giriniz:");
+                dizi[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] % m == 0)
+                {
+                    Console.Write(dizi[i]+" ");
+                }
+                    
+            }
+            Console.Write(" :"+m+"'e tam bölünen sayılardır");
+        }
+
+    }
+
+}
---
+++ AlgoritmaSorulari/soru3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgoritmaSoruları
+{
+    class birinciSoru
+    {
+        static void Main(string[] args)
+        {
+            /*Soru 3: Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+           Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+           Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın. */
+
+            Console.Write("Sıralanmasını istediğiniz kelime sayısını giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] dizi = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Lütfen " + (i + 1) + ".kelimeyi giriniz:");
+                dizi[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Girdiğiniz kelimelerin sıralanışı aşağıdaki gibidir: ");
+            Array.Reverse(dizi);
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            
+        }
+
+    }
+
+}
