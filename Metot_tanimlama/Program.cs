f5775de45bdfff55488b0f5c12883afd4b5890ae
yavuzpatika <114187938+yavuzpatika@users.noreply.github.com>
2022-11-07 00:26:43 +0300

    Metot_Tanimlama

---
+++ Metot_tanimlama/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+            
+            int sonuc = Topla(a, b);
+            Console.WriteLine("Topla methodu" + sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+            int sonuc2 = ornek.arttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+
+
+
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int arttirVeTopla (ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
